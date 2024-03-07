760f06fe478efcbb76a10b0bf57fb4d951f168b0
tuankiet091002 <tuankiet091002@gmail.com>
2024-03-07 14:52:48 +0700

    prerequisite_check added for keys that are bigger than half of the text's length. Comment latter

--- Rail Fence/RailFence.py
+++ Rail Fence/RailFence.py
@@ -1,3 +1,4 @@
+import math
 import re
 import time
 
@@ -44,6 +45,10 @@ def encrypt_rail_fence(text, rails):
 def decrypt_rail_fence(cipher, key):
     if key <= 1 or key >= len(cipher):
         return cipher
+
+    if key > math.ceil(len(cipher) / 2):
+        skip_index = 2 * key - len(cipher) - 1
+        return cipher[0: skip_index] + decrypt_rail_fence(cipher[skip_index:], key - skip_index)
 
     # matrix filled with \n to differentiate space with blank
     rail = [['\n' for i in range(len(cipher))] for j in range(key)]
@@ -128,35 +133,58 @@ def binary_search(arr, low, high, x):
         return -1
 
 
-def count_english_word(words):
-    matches = 0
+def check_english_word(word):
+    # regular expression check for uppercase character locations and special character
+    if not re.search("^[\"(]?[A-Za-z][a-z]*(-[a-z]+)*(\'([stdm]|ll))?[.,:;\")!?]?$", word):
+        return False
+    else:
+        # lowercase the word
+        processed_word = word.lower()
+        # remove acronym (n't, 'll, 'd, 's)
+        processed_word = re.sub('n\'t', '', processed_word)
+        processed_word = re.sub('\'([sdm]|ll)', '', processed_word)
+        # remove special character
+        processed_word = re.sub('[^A-Za-z0-9]+', '', processed_word)
 
-    for word in words:
-        # regular expression check for uppercase character locations and special character
-        if not re.search("^[\"(]?[A-Za-z][a-z]*(-[a-z]+)*(\'([stdm]|ll))?[.,:;\")!?]?$", word):
-            continue
+        if binary_search(ENGLISH_WORDS, 0, len(ENGLISH_WORDS) - 1, processed_word) != -1:
+            return True
+
+
+def prerequisite_check(text):
+    # high text length is required for stability(no proof)
+    if len(text) < 1000:
+        return 0
+
+    text_array = text.split(" ")
+    reverse_character_count = 0
+    score = 0
+
+    # guarantee last 20 words
+    i = 0
+    while i < len(text_array) and (score > 0.8 or i < 20):
+        word = text_array[i]
+
+        if check_english_word(word):
+            score = (score * i + 1) / (i + 1)
         else:
-            # lowercase the word
-            processed_word = word.lower()
-            # remove acronym (n't, 'll, 'd, 's)
-            processed_word = re.sub('n\'t', '', processed_word)
-            processed_word = re.sub('\'([sdm]|ll)', '', processed_word)
-            # remove special character
-            processed_word = re.sub('[^A-Za-z0-9]+', '', processed_word)
+            score = score * i / (i + 1)
 
-            if binary_search(ENGLISH_WORDS, 0, len(ENGLISH_WORDS) - 1, processed_word) != -1:
-                matches += 1
+        reverse_character_count += len(word)
+        i += 1
 
-    return matches
+    return reverse_character_count
 
 
-def check_is_english(text):
-    # sample_index = min(len(text), 999999)
+def get_english_score(text):
+    sample_index = min(len(text), 1000)
 
     # sorted word array of sampled text
     sample_array = text.split(" ")
-    english_count = count_english_word(sample_array)
-    # print("English count is: {0} words in total of {1} words.".format(english_count, len(sample_array)))
+    english_count = 0
+
+    for word in sample_array:
+        if check_english_word(word):
+            english_count += 1
 
     return english_count / len(sample_array)
 
@@ -165,19 +193,30 @@ def crack(message):
     # store list of all possible value
     score_list = []
     candidate_list = []
+    bound_offset = math.ceil(len(message) / 20)
+    print("Message length is {}".format(len(message)))
 
-    key_range = len(message)
-    key = 0
+    prerequisite_score = prerequisite_check(message) / len(message)
+    print("Prerequisite score is {}".format(prerequisite_score))
+    key_left_bound = math.ceil((prerequisite_score + 1) * len(message) / 2) \
+        if prerequisite_score > 0.2 else 0
+    key_right_bound = len(message) \
+        if (len(message) < 1000 or prerequisite_score > 0.2) \
+        else min(math.ceil((prerequisite_score + 1) * len(message) / 2) + bound_offset, len(message))
+
+    print("Left bound is {0}, right bound is {1}".format(key_left_bound, key_right_bound))
+
+    key = key_left_bound
     key_nearby = False
-    while key < key_range:
+    while key < key_right_bound:
         decrypted_text = decrypt_rail_fence(message, key)
-        score = check_is_english(decrypted_text)
+        score = get_english_score(decrypted_text)
 
         # set flag to stop the loop early
-        if score > 0.7:
+        if score > 0.85:
             if not key_nearby:
                 key_nearby = True
-                key_range = key + 25
+                key_right_bound = min(key + bound_offset * 2, len(message))
             candidate_list.append((key, score))
 
         score_list.append((key, score))
@@ -204,7 +243,8 @@ with open('plaintext.txt', 'r') as file:
     # Read the entire content of the file
     plain_text = file.read()
 
-encrypted_text = encrypt_rail_fence(plain_text, 120)
+encrypted_text = encrypt_rail_fence(plain_text, 5600)
 get_data()
+# print(prerequisite_check(encrypted_text))
 crack(encrypted_text)
 print("--- %s seconds ---" % (time.time() - start_time))
